2d2c03bbd324a0c2441792efe93f75a26372f91c
= <=>
2020-07-27 21:08:29 +0700

    ahihi

---
+++ src/main/java/com/linhnd/managerdevice/config/auth/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.linhnd.managerdevice.config.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    // Đoạn JWT_SECRET này là bí mật, chỉ có phía server biết
+    private final String JWT_SECRET = "tancute";
+
+    //Thời gian có hiệu lực của chuỗi jwt
+    private final long JWT_EXPIRATION = 604800000L;
+
+
+    public String generateToken(CustomUserDetail userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
+        // Tạo chuỗi json web token từ id của user.
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+
+    public String getEmailFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception ex) {
+
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/com/linhnd/managerdevice/config/auth/UserService.java
@@ -0,0 +1,29 @@
+package com.linhnd.managerdevice.config.auth;
+
+import com.linhnd.managerdevice.models.database.Users;
+import com.linhnd.managerdevice.repositorys.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService implements UserDetailsService {
+
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users users = usersRepository.getByEmail(username);
+        if (users == null){
+            throw new UsernameNotFoundException(username);
+        }
+
+       return new CustomUserDetail();
+    }
+}
